08e0783c7fee567c2fc486ab9f82ec0960a08b36
Falxns <maks.falonse@gmail.com>
2021-02-10 18:37:44 +0300

    added game page creation with handlebars

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const express = require ("express");
+const express = require("express");
+var exphbs = require("express-handlebars");
 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.get("/game/:id", function (req, res) {
+  const gameID = req.params.id;
+  res.render("game", {
+    title: "Dota 2",
+    imageUrl: "/assets/images/1.jpg",
+    platform: "PC",
+    genre: "Vizual Novel",
+    maturityRating: "4+",
+    price: "20",
+    desc:
+      "Lorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolor",
+  });
+});
 
 app.listen(3000, () => console.log("Server is listening.."));
